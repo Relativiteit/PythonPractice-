abe276e0c6257025c3168f6f8abcf35698eb7de1
Relativiteit <alejocain@gmail.com>
2020-10-28 16:11:41 +0100

    Add files via upload

---
+++ Module1/Hello World 1/hello_world2.py
@@ -0,0 +1,9 @@
+''' Assignment : hello_world2
+    Created on 23/10/2020
+    @author: Alejo Cain '''
+#input user
+name = input("Enter your name: ")
+#using %s as a placeholder
+print("Hello world!! written by: %s" % name)
+
+
