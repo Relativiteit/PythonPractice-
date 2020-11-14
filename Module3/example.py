b5dc7dc51f9fd719d59ac35a620760f27335b694
Relativiteit <alejocain@gmail.com>
2020-11-15 00:15:24 +0100

    opening files

--- Module3/example.py
+++ Module3/example.py
@@ -1,10 +1,8 @@
-NUMBER_OF_GRADES = 3
-
-with open("C:\\Users\\PourChevre\\PycharmProjects\\VUPythonProject\\Module3\\grades.txt") as file:
-    for line in file:
-        line = line.replace('_', ' ').split()
-        names, test_scores = line[:-NUMBER_OF_GRADES], line[-NUMBER_OF_GRADES:]
-        name = ' '.join(names)  # Join names together.
-        test_scores = [float(score) for score in test_scores]  # Make numeric.
-        average_score = sum(test_scores) / len(test_scores)
-        print('%s has an average grade of %.1f' % (name, average_score))
+# terrible heart example hahahah
+for row in range(6):
+    for col in range(7):  
+        if (row==0 and col %3 !=0)or(row==1 and col %3==0) or(row-col==2) or(row+col==8):  
+            print("*",end=" ")  
+        else:  
+            print(end=" ")  
+    print()  
