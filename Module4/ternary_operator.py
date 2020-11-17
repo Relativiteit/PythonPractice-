7e4238e72623f8a97f251f8c7b235773251a0a65
Relativiteit <alejocain@gmail.com>
2020-11-17 18:24:57 +0100

    ternary operator to check for largest number

---
+++ Module4/ternary_operator.py
@@ -0,0 +1,10 @@
+""" Assignment tenary_operator
+    Created on 17-11-2020, 18:20
+    @author Alejo Cain """
+
+a = int(input("Enter a number for a: "))
+b = int(input("Enter a number for b: "))
+
+print( a if a > b else b ) # compact if statement. Don't use to much to keep code readable.
+
+
