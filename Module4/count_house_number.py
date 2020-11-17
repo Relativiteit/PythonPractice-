98ba9814ba71761e93ef4938cd2e70de663a62f8
Relativiteit <alejocain@gmail.com>
2020-11-17 20:39:13 +0100

    reading data from file and parsing

---
+++ Module4/count_house_number.py
@@ -0,0 +1,10 @@
+""" Assignment count_house_number
+    Created on 17-11-2020, 19:42
+    @author Alejo Cain """
+def count_house_number(school, house_number)
+    students = school.splitlines
+school = open("input.txt").read().split("=")
+result = 0
+for school in schools:
+    result += count_house_numer(school, 3 )
+
