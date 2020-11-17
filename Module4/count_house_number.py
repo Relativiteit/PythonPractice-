7195a9f1e1a32c99efc934a56c6e99ad41d5327f
Relativiteit <alejocain@gmail.com>
2020-11-17 21:21:10 +0100

    added data in input file

--- Module4/count_house_number.py
+++ Module4/count_house_number.py
@@ -1,10 +1,30 @@
 """ Assignment count_house_number
     Created on 17-11-2020, 19:42
     @author Alejo Cain """
-def count_house_number(school, house_number)
+def read_house_number(address):
+    address = address.split(";")
+    house_number = int(address[1])
+    return house_number
+
+
+def has_house_number(student, house_number):
+    student = student.split("-")
+#    name = student[0] don't need name for now
+    address = student[1]
+    return read_house_number(address) == house_number
+
+
+def count_house_number(school, house_number):
     students = school.splitlines
+    result = 0
+
+    for student in students:
+        if has_house_numers(student, house_number):
+            result += 1 # local variable
+    return result
+
 school = open("input.txt").read().split("=")
-result = 0
+result = 0 # global variable
 for school in schools:
     result += count_house_numer(school, 3 )
 
