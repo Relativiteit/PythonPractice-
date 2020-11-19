c735d252502d445cebc121f1a6faab2e3541b074
Relativiteit <alejocain@gmail.com>
2020-11-19 13:08:43 +0100

    cahnged for line to line

--- Module3/geogrpahy_grades_1.py
+++ Module3/geogrpahy_grades_1.py
@@ -2,31 +2,26 @@
     Created on 14-11-2020, 20:12
     @author Alejo Cain """
 
-def convert_text_to_grades(text):
-    names = []
-    scores = []
-    for line in text:
-        name = ""
-        checking_name = True
-        score_text = ""
-        for letter in line:
-            if letter != "_" and checking_name:
-                name += letter
-            if letter == "_" and checking_name:
-                checking_name = False
-            if letter != "_" and not checking_name:
-                score_text += letter
+def convert_line_to_grades(line):
+    name = ""
+    checking_name = True
+    score_text = ""
+    for letter in line:
+        if letter != "_" and checking_name:
+            name += letter
+        if letter == "_" and checking_name:
+            checking_name = False
+        if letter != "_" and not checking_name:
+            score_text += letter
 
-        score_text = score_text.split(" ")
-        score_value = []
-        for score in score_text:
-            if(score == ''):
-                continue
-            score_value.append(float(score))
+    score_text = score_text.split(" ")
+    score_value = []
+    for score in score_text:
+        if(score == ''):
+            continue
+        score_value.append(float(score))
 
-        scores.append(score_value) # list(scores) with lists of grades(score_value)
-        names.append(name)
-    return (scores, names)
+    return (name, score_value)
 
 def average_grade(grades):
     average = 0
@@ -37,12 +32,12 @@ def average_grade(grades):
 
 # main
 text = open("grades.txt").read().split("\n")
-scores, names = convert_text_to_grades(text)
-
-# print
-for i, score in enumerate(scores):
-    if(len(score) == 0):
+for line in text:
+    name, score_value = convert_line_to_grades(line)
+    if(len(score_value) == 0):
         continue
+    average = average_grade(score_value)
+    print(str(name) + " %.2f" % average)
 
-    average = average_grade(score)
-    print(str(names[i]) + " %.2f" % average)
+
+
